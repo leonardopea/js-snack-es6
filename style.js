bd413a577bdf951ad7d575d84e0c93b5ed46e33c
leonardopea <leonardo.pea.1996@gmail.com>
2023-06-05 00:12:46 +0200

    snack 1 terminato

---
+++ style.js
@@ -0,0 +1,11 @@
+// SNACK 1
+
+const array = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin']
+
+
+array.forEach((elem,index) => {
+
+    console.log(`L'invitato ${elem} siede al posto ${index} del tavolo VIP`);
+
+
+})
